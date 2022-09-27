b9bcc10d53c5b754fe8166ec0165d16d9129e994
Bruno Oliveira <bluizsilva37@gmail.com>
2022-09-26 23:19:50 -0300

    atualização do dia 26/09/2022

--- app/src/main/java/com/example/firebase/TelaPrincipal.java
+++ app/src/main/java/com/example/firebase/TelaPrincipal.java
@@ -1,10 +1,27 @@
 package com.example.firebase;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class TelaPrincipal extends AppCompatActivity {
+
+    private TextView nomeUsuario,emailUsuario;
+    private Button bt_deslogar;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    String usuarioID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +29,43 @@ public class TelaPrincipal extends AppCompatActivity {
         setContentView(R.layout.activity_tela_principal);
 
         getSupportActionBar().hide();
+        IniciarComponentes();
 
+        bt_deslogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(TelaPrincipal.this,FormLogin.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        String email = FirebaseAuth.getInstance().getCurrentUser().getEmail();
+        usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        DocumentReference documentReference = db.collection("Usuarios").document(usuarioID);
+        documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshote, @Nullable FirebaseFirestoreException error) {
+                if (documentSnapshote != null){
+                    nomeUsuario.setText(documentSnapshote.getString("nome"));
+                    emailUsuario.setText(email);
+                }
+            }
+        });
+    }
+
+    private void IniciarComponentes(){
+
+        nomeUsuario = findViewById(R.id.textNomeUsuario);
+        emailUsuario = findViewById(R.id.textEmailUsuario);
+        bt_deslogar = findViewById(R.id.bt_deslogar);
     }
 }
